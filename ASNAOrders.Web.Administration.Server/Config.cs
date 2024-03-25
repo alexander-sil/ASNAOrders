61a598765be09b2a3ce15da50f235f6fe3348aaf
Alexander Silitskiy <sanya.silitskiy2@yandex.ru>
2024-03-25 11:48:33 +0900

    Add documentation

--- ASNAOrders.Web.Administration.Server/Config.cs
+++ ASNAOrders.Web.Administration.Server/Config.cs
@@ -2,8 +2,9 @@
 {
     public static class Config
     {
-
-
+        /// <summary>
+        /// Determines whether the token issuer signing key is generated manually.
+        /// </summary>
         public static bool IssuerSigningKeySetToManual { get; set; }
 
         public static string? IssuerSigningKey { get; set; }
