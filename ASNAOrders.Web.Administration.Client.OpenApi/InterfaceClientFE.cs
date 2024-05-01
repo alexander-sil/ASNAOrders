07566fb9216fb77b13bfb33329ce24239ae48dd8
sanya.silitskiy2 <sanya.silitskiy2@yandex.ru>
2024-05-01 11:44:28 +0300

    Refactor OpenAPI library the second time

---
+++ ASNAOrders.Web.Administration.Client.OpenApi/InterfaceClientFE.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASNAOrders.Web.Administration.Client.OpenApi
+{
+    internal class InterfaceClientFE
+    {
+        public InterfaceClient Client {  get; set; }
+
+        private static InterfaceClientFE? Instance { get; set; }
+
+        public static InterfaceClientFE GetInstance()
+        {
+            return Instance;
+        }
+
+        public InterfaceClientFE(string baseUrl, HttpClient client)
+        {
+            Instance = this;
+            Client = new InterfaceClient(baseUrl, client);
+        }
+    }
+}
